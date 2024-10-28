df6cc32c3b1f7dd4bd8e4be7ecffb22fdf5b7f15
bysanichaitanaya <bysanichaitanaya@gmail.com>
2024-10-28 15:50:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/React_Routes",
+  base:"/React_Classwork-Route_4F/",
   plugins: [react()],
+
 })
